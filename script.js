a829432b12e3f602a7fa3e1fb1b8ee93c2e98a42
Pedro <pedrohalmeidamendonca@gmail.com>
2022-09-03 15:45:04 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const textInput = document.getElementById('text-input');
 const memeImageContainer = document.getElementById('meme-image-container');
+const memeImage = document.getElementById('meme-image');
 
 function elementText() {
   document.getElementById('meme-text').innerText = textInput.value;
@@ -8,7 +9,7 @@ function elementText() {
 function uploadImage(event) {
   const reader = new FileReader();
   reader.onload = () => {
-    const output = document.getElementById('meme-image');
+    const output = memeImage;
     output.src = reader.result;
   };
   reader.readAsDataURL(event.target.files[0]);
@@ -26,8 +27,28 @@ function earthBoard() {
   memeImageContainer.style.border = '6px groove rgb(0, 128, 0)';
 }
 
+function insertMeme1() {
+  memeImage.src = './imgs/meme1.png';
+}
+
+function insertMeme2() {
+  memeImage.src = './imgs/meme2.png';
+}
+
+function insertMeme3() {
+  memeImage.src = './imgs/meme3.png';
+}
+
+function insertMeme4() {
+  memeImage.src = './imgs/meme4.png';
+}
+
 textInput.addEventListener('keyup', elementText);
 textInput.addEventListener('change', uploadImage);
 document.getElementById('fire').addEventListener('click', fireBoard);
 document.getElementById('water').addEventListener('click', waterBoard);
 document.getElementById('earth').addEventListener('click', earthBoard);
+document.getElementById('meme-1').addEventListener('click', insertMeme1);
+document.getElementById('meme-2').addEventListener('click', insertMeme2);
+document.getElementById('meme-3').addEventListener('click', insertMeme3);
+document.getElementById('meme-4').addEventListener('click', insertMeme4);
